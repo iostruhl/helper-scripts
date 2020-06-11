1d790edc5e406daf6c2280b88612b071b4391dd1
Isaac Struhl <iostruhl@gmail.com>
2020-06-11 17:19:12 -0400

    minor edit to shell script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Creating ~/.vim directories..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 
 echo "Installing pathogen..."
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "Downloading vimrc..."
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/iostruhl/vim-setup/master/.vimrc
