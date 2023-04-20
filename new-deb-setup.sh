18bb676b47dfc7afd1ddbe24635f87551d00a98a
Isaac Struhl <iostruhl@gmail.com>
2023-04-20 15:48:10 -0400

    fixed new-deb

--- new-deb-setup.sh
+++ new-deb-setup.sh
@@ -8,12 +8,12 @@ sudo apt install git curl vim
 # Vim setup, aliases, and other quality of life.
 curl -SsL iostruhl.com/vimsetup/install.sh | bash
 echo "set completion-ignore-case on" >> ~/.inputrc
-echo >> ~/.profile
-echo '# User defined aliases.' >> ~/.profile
-echo 'alias l="ls -lGgh"' >> ~/.profile
-echo 'alias la="ls -lGgah"' >> ~/.profile
-echo 'alias mka="vim ~/.profile"' >> ~/.profile
-echo 'alias src="source ~/.profile"' >> ~/.profile
+echo >> ~/.bashrc
+echo '# User defined aliases.' >> ~/.bashrc
+echo 'alias l="ls -lGgh"' >> ~/.bashrc
+echo 'alias la="ls -lGgah"' >> ~/.bashrc
+echo 'alias mka="vim ~/.bashrc"' >> ~/.bashrc
+echo 'alias src="source ~/.bashrc"' >> ~/.bashrc
 
 # Install python build dependencies.
 sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
@@ -21,17 +21,14 @@ sudo apt-get install --no-install-recommends make build-essential libssl-dev zli
 # Install pyenv and pyenv-virtualenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-echo >> ~/.profile
-echo 'PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-echo 'PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-echo >> ~/.profile
-echo 'eval "$(pyenv init -)"' >> ~/.profile
-
-exec "$SHELL"
+echo >> ~/.bashrc
+echo 'PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-echo >> ~/.profile
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.profile
+echo >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
-exec "$SHELL"
-
+source ~/.bashrc
