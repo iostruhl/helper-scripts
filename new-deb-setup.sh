fc3f1a049a2af995df0df437b05e61524a7e17fa
Isaac Struhl <iostruhl@gmail.com>
2023-09-19 10:31:54 -0400

    updated again

--- new-deb-setup.sh
+++ new-deb-setup.sh
@@ -5,27 +5,34 @@ set -euxo pipefail
 # Install deps for initial setup and python.
 sudo apt-get update && sudo apt-get install git curl vim make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
-# Vim setup, aliases, and other quality of life.
-curl -SsL iostruhl.com/vimsetup/install.sh | bash
-echo "set completion-ignore-case on" >> ~/.inputrc
-echo >> ~/.bashrc
-echo '# User defined aliases.' >> ~/.bashrc
-echo 'alias l="ls -lGgh"' >> ~/.bashrc
-echo 'alias la="ls -lGgah"' >> ~/.bashrc
-echo 'alias mka="vim ~/.bashrc"' >> ~/.bashrc
-echo 'alias src="source ~/.bashrc"' >> ~/.bashrc
-
-# Install pyenv and pyenv-virtualenv
+# Install pyenv.
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-echo >> ~/.bashrc
-echo 'PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-echo >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+# Vim config.
+curl -SsL iostruhl.com/vimsetup/install.sh | bash
+
+# Input tab completion config.
+echo "set completion-ignore-case on" >> ~/.inputrc
+
+
+echo '
+# User defined aliases.
+alias l="ls -lGgh"
+alias la="ls -lGgah"
+
+# Pyenv
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ (\1)/"
+}
+export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[31m\]$(parse_git_branch)\[\033[00m\]"
+' >> ~/.bashrc
 
 source ~/.bashrc
+
