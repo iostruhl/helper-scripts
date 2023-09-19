480e5edeb2ca9f46fba854078a0194faa442ba12
Isaac Struhl <iostruhl@gmail.com>
2023-09-19 10:55:23 -0400

    git config

--- new-deb-setup.sh
+++ new-deb-setup.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 # Install deps for initial setup and python.
 sudo apt-get update && sudo apt-get install git curl vim make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
 # Install pyenv.
-if [ -d "~/.pyenv" ]; then
+if [ -d ~/.pyenv ]; then
   rm -rf ~/.pyenv
 fi
 
@@ -37,6 +37,11 @@ parse_git_branch() {
 }
 export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[31m\]$(parse_git_branch)\[\033[00m\] "
 ' >> ~/.bashrc
+
+# Git config, storing revocable creds.
+git config --global user.name "Isaac Struhl"
+git config --global user.email "isaac@distributedspectrum.com"
+git config --global credential.helper store
 
 source ~/.bashrc
 
